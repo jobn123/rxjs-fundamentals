7e9579ee7d146da27aee7cb556ec261cb69db985
jobn123 <jobn_joy@163.com>
2022-10-13 16:02:25 +0800

    feat: finish innterval test

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -3,3 +3,17 @@ import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+
+let tick$ = interval(1000);
+let subscription$;
+
+start$.subscribe(() => {
+
+  if (subscription$) {
+    subscription$.unsubscribe();
+  }
+
+  subscription$ = tick$.subscribe(setCount)
+})
+
+pause$.subscribe(() => subscription$.unsubscribe())
