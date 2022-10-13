59cc55c5f612360ea7d8f372035500bb837496db
jobn123 <jobn_joy@163.com>
2022-10-13 18:19:44 +0800

    feat: finish merging-timelines

--- applications/merging-timelines/script.js
+++ applications/merging-timelines/script.js
@@ -7,3 +7,41 @@ import {
   setStatus,
   bootstrap,
 } from './utilities';
+
+// examples1
+// const start$ = fromEvent(startButton, 'click').pipe(mapTo(true))
+// const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false))
+
+// const isRunning$ = merge(start$, pause$).pipe(startWith(false))
+
+// isRunning$.subscribe(setStatus)
+
+// examples2
+// const start$ = fromEvent(startButton, 'click').pipe(mapTo(true))
+// const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false))
+
+// const isRunning$ = merge(start$, pause$).pipe(startWith(false))
+
+// isRunning$.subscribe(setStatus)
+
+// const first$ = interval(1000).pipe(map(labelWith('First')), take(4))
+// const second$ = interval(1000).pipe(map(labelWith('Second')), take(4))
+// const combined$ = interval(1000).pipe(map(labelWith('Combined')), take(4))
+
+// bootstrap({ first$, second$, combined$ })
+
+const start$ = fromEvent(startButton, 'click').pipe(mapTo(true))
+const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false))
+
+const isRunning$ = merge(start$, pause$).pipe(startWith(false))
+
+isRunning$.subscribe(setStatus)
+
+const first$ = interval(1000).pipe(map(labelWith('First')), take(4))
+const second$ = interval(1000).pipe(map(labelWith('Second')), take(4))
+// const combined$ = merge(first$, second$)
+// const combined$ = concat(first$, second$)
+// const combined$ = race(first$, second$)
+const combined$ = forkJoin(first$, second$)
+
+bootstrap({ first$, second$, combined$ })
